5d0265562f5f8e4fdf6f7d2840b40b86fb83b587
Gregoire Lamy <g.aderlamy@gmail.com>
2019-10-22 21:36:46 +0200

    canvasPeuAvancé

--- starter_code/canvas.js
+++ starter_code/canvas.js
@@ -2,35 +2,49 @@
 class HangmanCanvas {
   constructor(secretWord) {
     this.ctx = document.getElementById('hangman').getContext('2d');
-    
+    this.secretWord=secretWord; 
+    this.createBoard();
+    this.drawLines();
   }
 
+
   createBoard() {
+    this.ctx.clearRect(0, 0, 800, 1200); 
+    this.ctx.lineWidth = 5;
 
   }
 
   drawLines() {
-
+    var longueur = 20 ;
+    var depart = 100 ; 
+    for (var i = 0 ; i < this.secretWord.length ; i++){
+      this.ctx.beginPath();
+      this.ctx.moveTo(depart, 600);
+      this.ctx.lineTo(depart+longueur, 600);
+      depart += 5 ; 
+      this.ctx.stroke();
+    }
+        
   }
 
-  writeCorrectLetter(index) {
+//   writeCorrectLetter(index) {
 
-  }
+//   }
 
-  writeWrongLetter(letter, errorsLeft) {
+//   writeWrongLetter(letter, errorsLeft) {
 
-  }
+//   }
 
-  drawHangman(shape) {
+//   drawHangman(shape) {
 
-  }
+//   }
 
-  gameOver() {
+//   gameOver() {
 
-  }
+//   }
 
-  winner() {
+//   winner() {
 
-  }
+//   }
 
 }
--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,44 +1,110 @@
-let hangman;
+// let hangman;
+
+function code2letter(code) {
+  //
+  // function that convert a keycode into a letter (see: https://keycode.info)
+  //
+  return "ABCDEFGHIJKLMNOPQRSTUVWXYZ"[code - 65]; 
+}
+
+class Hangman {
+  constructor() {
+    this.words = ['IRONHACK', 'IRONHACK', 'IRONHACK'];
+    this.secretWord = this.getWord();
+    this.letters = [];
+    this.guessedLetter = "";
+    this.errorsLeft = 10;
+  }
+
+  getWord() {
+    return this.words[Math.floor(Math.random()*this.words.length)];
+  }
+
+  checkIfLetter(keyCode) {
+    const letter = code2letter(keyCode);
+
+    return letter ? true : false;
+  }
+
+  checkClickedLetters(letter) {
+    const notAlreadyTyped = this.letters.indexOf(letter) === -1; // can't find `letter` into `this.letters`
+
+    if (notAlreadyTyped) {
+      this.letters.push(letter);
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  addCorrectLetter(index) {
+    const correctLetter = this.secretWord[index];
+
+    this.guessedLetter += correctLetter.toUpperCase(); // store the uppercase version
+  }
+
+  addWrongLetter(letter) {
+    this.errorsLeft -= 1;
+    //this.checkGameOver();
+  }
+
+  checkGameOver() {
+    return (this.errorsLeft < 1);
+  }
+
+  checkWinner() {
+    return this.guessedLetter.toUpperCase().split('').sort().join('') === this.secretWord.toUpperCase().split('').sort().join('');
+  }
+
+}
+
+// il reste les fonctions add correct et add wrong à faire, le reste fonctionne 
 
 // class Hangman {
-//   constructor() {
+//    constructor() {
+//       this.words=['bonjour','bonsoir', 'salut']; 
+//       this.secretWord=''; 
+//       this.letters=[];
+//       this.guessedLetter=''; 
+//       this.errorsLeft=10;
 
-//   }
+//    }
 
-//   getWord() {
+//     getWord() {
+//       this.secretWord = this.words[Math.floor(Math.random()*this.words.length)]; 
+//       return this.secretWord; 
+//     }
 
-//   }
 
-//   checkIfLetter(keyCode) {
+//     checkIfLetter(keyCode) {
+//       if (keyCode>64 && keyCode<91){
+//         return true ;
+//       } else return false ; 
+//     }
+ 
+//     checkClickedLetters(key) {
+//       if (this.letters.indexOf(key)===-1) return true ; 
+//         else return false; 
+//       }
 
-//   }
+//     checkGameOver() {
+//         if (this.errorsLeft===0) return true ;
+//           else return false; 
+//       }
 
-//   checkClickedLetters(key) {
+//     checkWinner() {
+//       if (this.secretWord.length===this.guessedLetter.length) return true ; 
+//         else return false ;   
+//       }
 
-//   }
-
-//   addCorrectLetter(i) {
-
-//   }
-
-//   addWrongLetter(letter) {
-
-//   }
-
-//   checkGameOver() {
-
-//   }
-
-//   checkWinner() {
-
-//   }
-
+//     addCorrectLetter(index) {
+//         const correctLetter = this.secretWord[index];
+//         this.guessedLetter += correctLetter.toUpperCase(); 
+//       }
+    
+//     addWrongLetter(letter) {
+//         this.errorsLeft -= 1;
+//         this.checkGameOver();
+//       }
+//   ​
 // }
-
-document.getElementById('start-game-button').onclick = () => {
-  hangman = new Hangman();
-};
-
-document.onkeydown = (e) => {
-
-};

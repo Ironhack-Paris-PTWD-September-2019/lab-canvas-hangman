ec045a490ecdd1521d57113e3b1ec2b0112b5d1b
Antoine BERNIER <antoine.bernier@gmail.com>
2019-10-22 17:33:16 +0200

    Create main.js

---
+++ starter_code/main.js
@@ -0,0 +1,19 @@
+var hangman;
+var hangmanCanvas;
+
+var $start = document.getElementById('start-game-button');
+$start.onclick = function () {
+  console.log('starting');
+  
+  hangman = new Hangman();
+  hangmanCanvas = new HangmanCanvas(hangman.secretWord)
+};
+
+document.onkeydown = (e) => {
+  const code = e.which;
+  console.log('code', code);
+
+  //
+  // your turn ;)
+  //
+};

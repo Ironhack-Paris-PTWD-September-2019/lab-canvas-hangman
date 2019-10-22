f164977376173f61329b71142ae38439e77068db
Sandrine8304 <sandrine.auberval@gmail.com>
2019-10-22 21:34:53 +0200

    1er essai

--- starter_code/canvas.js
+++ starter_code/canvas.js
@@ -1,36 +1,34 @@
-
 class HangmanCanvas {
   constructor(secretWord) {
-    this.ctx = document.getElementById('hangman').getContext('2d');
-    
+    this.ctx = document.getElementById("hangman").getContext("2d");
+    this.createBoard();
+    this.drawLines();
   }
 
   createBoard() {
-
+    //this.ctx.fillStyle = "red";
+    this.ctx.clearRect(0, 0, 800, 1200);
   }
 
   drawLines() {
+    var path = ctx.beginPath();
 
+    for (let i = 0; i < this.secretWord.length; i++) {
+      this.ctx.beginPath(700, 100);
+      this.ctx.lineTo(700, 100);
+      // this.ctx.stroke();
+      // this.ctx.moveTo();
+      //this.ctx.beginPath();
+    }
   }
 
-  writeCorrectLetter(index) {
+  writeCorrectLetter(index) {}
 
-  }
+  writeWrongLetter(letter, errorsLeft) {}
 
-  writeWrongLetter(letter, errorsLeft) {
+  drawHangman(shape) {}
 
-  }
+  gameOver() {}
 
-  drawHangman(shape) {
-
-  }
-
-  gameOver() {
-
-  }
-
-  winner() {
-
-  }
-
+  winner() {}
 }
--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,44 +1,120 @@
-let hangman;
+// let hangman;
 
 // class Hangman {
 //   constructor() {
-
+//     this.words = ["coucou", "bonjour", "au revoir"];
+//     this.secretWord = this.getWord();
+//     this.letters = [];
+//     this.guessedLetters = "";
+//     this.errorsLeft = 10;
 //   }
 
 //   getWord() {
-
+//     return this.words[Math.floor(Math.random() * this.words.length)];
 //   }
 
 //   checkIfLetter(keyCode) {
-
+//     let checkLetter = false;
+//     if (keyCode > 64 && keyCode < 91) {
+//       checkLetter = true;
+//       return checkLetter;
+//     } else {
+//       return checkLetter;
+//     }
 //   }
 
 //   checkClickedLetters(key) {
-
+//     let letterUsed = true;
+//     if (this.letters.indexOf(key) === -1) {
+//       return letterUsed;
+//     } else {
+//       letterUsed = false;
+//       return letterUsed;
+//     }
 //   }
 
-//   addCorrectLetter(i) {
+//   //   addCorrectLetter(i) {
 
-//   }
+//   //   }
 
-//   addWrongLetter(letter) {
+//   //   addWrongLetter(letter) {
 
-//   }
+//   //   }
 
-//   checkGameOver() {
+//   //   checkGameOver() {
 
-//   }
+//   //   }
 
-//   checkWinner() {
+//   //   checkWinner() {
 
-//   }
+//   //   }
 
 // }
 
-document.getElementById('start-game-button').onclick = () => {
-  hangman = new Hangman();
-};
+function code2letter(code) {
+  //
+  // function that convert a keycode into a letter (see: https://keycode.info)
+  //
+  return "ABCDEFGHIJKLMNOPQRSTUVWXYZ"[code - 65];
+}
 
-document.onkeydown = (e) => {
+class Hangman {
+  constructor() {
+    this.words = ["IRONHACK", "IRONHACK", "IRONHACK"];
+    this.secretWord = this.getWord();
+    this.letters = [];
+    this.guessedLetter = "";
+    this.errorsLeft = 10;
+  }
 
-};
+  getWord() {
+    return this.words[Math.floor(Math.random() * this.words.length)];
+  }
+
+  checkIfLetter(keyCode) {
+    const letter = code2letter(keyCode);
+
+    return letter ? true : false;
+  }
+
+  checkClickedLetters(letter) {
+    const notAlreadyTyped = this.letters.indexOf(letter) === -1; // can't find `letter` into `this.letters`
+
+    if (notAlreadyTyped) {
+      this.letters.push(letter);
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  addCorrectLetter(index) {
+    const correctLetter = this.secretWord[index];
+
+    this.guessedLetter += correctLetter.toUpperCase(); // store the uppercase version
+  }
+
+  addWrongLetter(letter) {
+    this.errorsLeft -= 1;
+    //this.checkGameOver();
+  }
+
+  checkGameOver() {
+    return this.errorsLeft < 1;
+  }
+
+  checkWinner() {
+    return (
+      this.guessedLetter
+        .toUpperCase()
+        .split("")
+        .sort()
+        .join("") ===
+      this.secretWord
+        .toUpperCase()
+        .split("")
+        .sort()
+        .join("")
+    );
+  }
+}

7ed531ef6c5a46e48b131df10afc1ae6501e0408
martinezzzzzzz <55624848+martinezzzzzzz@users.noreply.github.com>
2019-10-22 21:38:28 +0200

    done

--- starter_code/canvas.js
+++ starter_code/canvas.js
@@ -2,9 +2,7 @@
 class HangmanCanvas {
   constructor(secretWord) {
     this.ctx = document.getElementById('hangman').getContext('2d');
-    
-    
-    
+  
   }
 
   createBoard() {
@@ -12,11 +10,10 @@ class HangmanCanvas {
   }
 
   drawLines() {
-
     this.ctx.beginPath();
-    this.ctx.rect(0, 0, 800, 1200);
-    this.ctx.fillStyle = "red";
-    this.ctx.fill();
+    this.ctx.moveTo(50, 50);
+    this.ctx.lineTo(250, 50);
+    this.ctx.stroke();
     this.ctx.closePath();
   }
 
